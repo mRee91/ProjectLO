0c1f52919b0f6579d137d9d92cd52d5ad63cfe28
mRee91 <no.1@gmx.com>
2022-06-01 19:39:54 +0100

    second commit

---
+++ PrimeNumber.java
@@ -0,0 +1,35 @@
+package Project;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+
+		Scanner inp = new Scanner (System.in);
+		
+		int a= inp.nextInt(), q=0; String prime="null", con="null";
+		
+		for (int b=2; b<a; b++) {
+			
+			if (q>2) con="confedfs"; else con="not condd";
+			
+			if(a%b==0) q+=1;
+			if(a%b!=0)prime="a prime";
+			else {prime="not a prime"; break;}        }
+		
+
+			System.out.println("the number entered is "+ prime+" and is.. "+con);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
